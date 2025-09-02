07a0b1dee70103e52b45f85650b40f47cdb33d5c
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2025-09-03 07:39:49 +0800

    Finish 08b-local

---
+++ htc/08b-local/lecture/evaluation_with_locals.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname evaluation_with_locals) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define b 1)
+
+(+ b
+   (local [(define b 4)]
+     (* b b))
+   b)
